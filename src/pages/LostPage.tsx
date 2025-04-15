f46336528f40a29d0c3c7b76cacecbf4c5529449
eunbeen <dmsqls0704@naver.com>
2025-04-15 23:26:02 +0900

    feat: naverMap API 연결

--- src/pages/LostPage.tsx
+++ src/pages/LostPage.tsx
@@ -1,13 +1,24 @@
 import React, {useState} from "react";
 import { View, StyleSheet } from "react-native";
+import { NaverMapView } from  '@mj-studio/react-native-naver-map';
 import MenuBar from '../components/MenuBar';
+
+const INITIAL_CAMERA = {
+  latitude: 37.5666102,  // 서울 중심부 위도
+  longitude: 126.9783881,  // 서울 중심부 경도
+  zoom: 12,  // 줌 레벨
+};
 
 const LostPage = () => {
     const [currentTab, setCurrentTab] = useState('Lost');
     return (
         <View style={styles.mainContainer}>
             <View style={styles.contentContainer}>
-            
+            <NaverMapView
+              style={{ width: '100%', height: '100%' }}
+              initialCamera={INITIAL_CAMERA}
+            />
+
             </View>
             <MenuBar currentTab={currentTab} onTabPress={setCurrentTab} />
         </View> 
